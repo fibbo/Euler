8aacfececa71d45eb544a49a7a914db3792ad1c7
fibbo <siebesiech@gmail.com>
2015-09-22 17:44:25 +0200

    problem 5

---
+++ 005/problem5.py
@@ -0,0 +1,5 @@
+for x in range(2520,1000000,20):
+	print x
+  if not x%19 and not x%18 and not x%17 and not x%16 and not x%15 and not x%14 and not x%13 and not x%12 and not x%11:
+  	print x
+  	break

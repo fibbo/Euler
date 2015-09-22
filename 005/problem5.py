e0cfadda70d244e42ae1b9b14d2205ea8258b887
Philipp Gloor <pgloor@lxplus0039.cern.ch>
2015-09-22 18:54:22 +0200

    updates

--- 005/problem5.py
+++ 005/problem5.py
@@ -1,5 +1,6 @@
-for x in range(2520,1000000,20):
-	print x
+x = 2520
+while True:  
   if not x%19 and not x%18 and not x%17 and not x%16 and not x%15 and not x%14 and not x%13 and not x%12 and not x%11:
-  	print x
-  	break
+    print x
+    break
+  x += 20
